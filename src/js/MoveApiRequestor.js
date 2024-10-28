39f117764a1295e040347354f2e148f8a2ce9c45
ShrayzzDev <renaudflash@gmail.com>
2024-10-28 21:15:27 +0100

    [MOD] MoveApiRequestor using the generic requestor

--- src/js/MoveApiRequestor.js
+++ src/js/MoveApiRequestor.js
@@ -1,22 +1,8 @@
-const API_URL = "https://localhost:7271"
+import {GenericApiRequestor} from "./GenericApiRequestor.js";
 
 export async function moveListFromPokemon(pkmnId, generation, level, action) {
     try {
-        const response = fetch(
-            `${API_URL}/pokemon/${pkmnId}/Moves/${generation}g/${level}`
-        );
-
-        response.then(
-            async resp => {
-                await resp.text().then(
-                    text => action(text))
-            }
-        ).catch(
-            error => {
-                console.log(error)
-            }
-        );
-
+        await GenericApiRequestor(`/pokemon/${pkmnId}/Moves/${generation}g/${level}`, action);
     } catch (error) {
         console.log(error.message);
     }
